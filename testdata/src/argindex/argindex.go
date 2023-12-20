0ef04a4184845aa9d0bae170afda8f1066b39976
nametake <nametake.kyarabuki@gmail.com>
2023-12-21 01:33:06 +0900

    Added testcase

--- testdata/src/argindex/argindex.go
+++ testdata/src/argindex/argindex.go
@@ -9,5 +9,11 @@ func f2(num1, num2 int) { // want "func f2 not found arg string"
 func f3(str string) { // want "func f3 not found arg string"
 }
 
-func f4(num int, str string) {
+func f4(str1, str2 string) {
+}
+
+func f5(num int, str string) {
+}
+
+func f6(num int, str string, b bool) {
 }
